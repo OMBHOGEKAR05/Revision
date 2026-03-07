0e2e1684549dc619cdda515a9c9493dab7cacbc1
OMBHOGEKAR05 <ombhogekar1@gmail.com>
2026-03-07 12:35:35 +0530

    Added change color opitimized program

--- promise.js
+++ promise.js
@@ -17,24 +17,66 @@
 // })
 
 
-function saveToDb(data, success, failure){
-    let network = Math.floor(Math.random() * 10) + 1;
-    if(network > 4){
-        success();
-    }else{
-        failure();
-    }
+// function saveToDb(data, success, failure){
+//     let network = Math.floor(Math.random() * 10) + 1;
+//     if(network > 4){
+//         success();
+//     }else{
+//         failure();
+//     }
+// }
+
+
+// saveToDb("ApnaCollege", ()=>{
+//     console.log("Your data was saved...");
+//     saveToDb("hello world!!" , ()=>{
+//         console.log("Your data was saved...");
+//     } ,
+//     ()=>{
+//         console.log("Weak connection, Try later");
+//     })
+// }, ()=>{
+//     console.log("Weak connection, Try later");
+// })
+
+
+// function saveToDb(data){
+//     return new Promise((resolve, reject)=>{
+//         let internetSpeed = Math.floor(Math.random() * 10) + 1;
+//         if(internetSpeed > 4){
+//             resolve("Successful");
+//         }else{
+//             reject("Failed");
+//         }
+//     })
+// }
+
+
+
+let h1 = document.querySelector("h1");
+
+function changecolor(color){
+    return new Promise((resolve, reject)=>{
+        setTimeout(()=>{
+            h1.style.color = color;
+            resolve(color);
+        },1000)
+    })
 }
 
-
-saveToDb("ApnaCollege", ()=>{
-    console.log("Your data was saved...");
-    saveToDb("hello world!!" , ()=>{
-        console.log("Your data was saved...");
-    } ,
-    ()=>{
-        console.log("Weak connection, Try later");
-    })
-}, ()=>{
-    console.log("Weak connection, Try later");
+changecolor("Red")
+.then((color)=>{
+    console.log("Colour changed to ,",color);
+    return changecolor("blue")
 })
+.then((color)=>{
+    console.log("Colour changed to ,",color);
+    return changecolor("Orange")
+})
+.then((color)=>{
+    console.log("Colour changed to ,",color);
+})
+.catch(()=>{
+    console.log("Colours are finished!");
+})
+
